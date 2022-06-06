6e93d8d74d44392b4336dfd9b1b943b9379605dc
Mark Bolwell <mark.bollyuk@gmail.com>
2022-06-06 10:59:45 +0100

    updated with amazon specifc
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -25,7 +25,7 @@ AUDIT_CONTENT_LOCATION="${AUDIT_CONTENT_LOCATION:-/var/tmp}"  # Location of the 
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=CIS  # Benchmark Name aligns to the audit
 BENCHMARK_VER=2.0.0
-BENCHMARK_OS=RHEL8
+BENCHMARK_OS=AmazonLinux2  # This is also used to set os_vendor for AmazonLinux2
 
 
 
@@ -80,7 +80,10 @@ fi
 
 # Discover OS version aligning with audit
 # Define os_vendor variable
-if [ `grep -c rhel /etc/os-release` != 0 ]; then
+if [[ "$BENCHMARK_OS" == AmazonLinux2 ]]; then
+   os_vendor="AMAZON"
+elif
+   [ `grep -c rhel /etc/os-release` != 0 ]; then
     os_vendor="RHEL"
 else
     os_vendor=`hostnamectl | grep Oper | cut -d : -f2 | awk '{print $1}' | tr a-z A-Z`
