b5bb8ea715d6e591ebb5f986db2502ed631ca361
Mark Bolwell <mark.bollyuk@gmail.com>
2025-10-07 13:35:58 +0100

    updated OS variables
    Signed-off-by: Mark Bolwell <mark.bollyuk@gmail.com>

--- run_audit.sh
+++ run_audit.sh
@@ -27,8 +27,7 @@
 # Goss benchmark variables (these should not need changing unless new release)
 BENCHMARK=CIS # Benchmark Name aligns to the audit
 BENCHMARK_VER=1.0.0
-BENCHMARK_OS=RHEL10
-
+BENCHMARK_OS=AMAZON2
 # Goss host Variables
 AUDIT_BIN="${AUDIT_BIN:-/usr/local/bin/goss}"  # location of the goss executable
 AUDIT_BIN_MIN_VER="0.4.4"
